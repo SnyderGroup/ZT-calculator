d22fb20df06cf40bc298a07812c88b598c12390c
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2020-10-26 13:01:23 -0500

    Cleened up plotingZT

--- plotingZT.py
+++ plotingZT.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 plt.rc('font', family='serif', size=20)
 import csv
 import math
-def Import(file_name):
-    file = []
-    with open(file_name, 'r') as opended_File:
-        File = csv.reader(opended_File)
-        for row in File:
+def Import(file_pathname):
+    data = []
+    with open(file_pathname, 'r') as opened_File:
+        CSV_File = csv.reader(opened_File)
+        for row in CSV_File:
             #temporarily store the values in each row before adding it to the completed file
             store_row=[]
             # if there is stuff in the row
@@ -18,8 +18,8 @@ def Import(file_name):
                         # if the value exists
                         if len(value) > 0:
                             store_row.append(float(value))
-                    file.append(store_row)
-    opended_File.close()
+                    data.append(store_row)
+    opened_File.close()
     return (file)
 
 def Export(file_name, data):
@@ -31,38 +31,38 @@ def Export(file_name, data):
             File.write(str(num))
             File.write(",")
     File.close()
-def optomiseUfordefeff(data):
+def optimizedU(data):
     Us = [.1, 4]
     while True:
-        ux = find_highest(Us)
-        um = find_lowest(Us)
-        if abs(um-ux) < .000001:
+        uMax = max(Us)
+        uMin = min(Us)
+        if uMax-Min < .000001:
             break
         test_U = []
-        du = (ux-um)/5
+        du = (uMax-uMin)/5
         for x in range(6):
-            test_U.append(functions_efficiency_as_a_function_of_u(data, (um+x*du)))
-        place = find_place(find_highest(test_U), test_U)
+            test_U.append(functions_efficiency_as_a_function_of_u(data, (uMin+x*du)))
+        place = find_place(max(test_U), test_U)
         Us = []
-        Us.append(um + du*place)
+        Us.append(uMin + du*place)
         test_U[place] = -100000
-        Us.append(um + du*find_place(find_highest(test_U), test_U))
-    return (um)
+        Us.append(uMin + du*find_place(max(test_U), test_U))
+    return (uMin)
 
-def find_place(number, place):
+def find_place(number, place): # find index (value, litslike)
     for thing in range(len(place)):
         #print (thing, place[thing])
         if place[thing] == number:
             return thing
     print ("Error in find_place")
-def find_highest(place):
+def find_highest(place): #max()
     x = place[0]
     #print(place)
     for number in place:
         if number > x:
             x = number
     return (x)
-def find_lowest(place):
+def find_lowest(place): #min()
     x = place[0]
     for number in place:
         if number < x:
@@ -74,17 +74,16 @@ def functions_zT(T,R,S,K):
 def functions_max_Red_eff(zT):
     return ((math.sqrt(1+zT)-1)/(math.sqrt(1+zT)+1))
 
-def functions_efficiency_as_a_function_of_u(file, initial_u):
-    for datum in (file):
-        if len(datum) < 8:
-            datum.append(0)
-    file[0][7] = (initial_u)
+def functions_efficiency_as_a_function_of_u(file, initial_u): # efficiency from u int(file , u_int):
+    # predicts the efficiency of the total material form a given starting u.
+    Us = [None for n in file]
+    Us[0] = initial_u
     for datum in range(len(file)-1):
-        file[datum + 1][7] = (1/(((1/file[datum][7])*math.sqrt(abs(1-2*file[datum][7]*file[datum][7]*(file[datum+1][1]*file[datum+1][3]+file[datum][1]*file[datum][3])*(10**-5)/2*(file[datum+1][0]-file[datum][0]))))-(file[datum+1][0]+file[datum][0])/2*(file[datum+1][2]-file[datum][2])*(10**-6)))
-    NL = file[-1][2]*file[-1][0]/1000000+1/file[-1][7]
-    N1 = file[0][2]*file[0][0]/1000000+1/file[0][7]
+        Us[datum + 1] = (1/(((1/Us[datum])*math.sqrt(abs(1-2*Us[datum]*Us[datum]*(file[datum+1][1]*file[datum+1][3]+file[datum][1]*file[datum][3])*(10**-5)/2*(file[datum+1][0]-file[datum][0]))))-(file[datum+1][0]+file[datum][0])/2*(file[datum+1][2]-file[datum][2])*(10**-6)))
+    NL = file[-1][2]*file[-1][0]/1000000+1/Us[-1]
+    N1 = file[0][2]*file[0][0]/1000000+1/Us[0]
     (NL-N1)/NL
-    return((NL-N1)/NL)
+    return((NL-N1)/NL) #final - initial
 
     #return(eachZT)
 #temperature(T) is in Kelvin
@@ -93,13 +92,14 @@ def functions_efficiency_as_a_function_of_u(file, initial_u):
 #thermal conductivity(K) is in Watts/(Meter*Kelvin)*10^-1
 
 def ploting_rangeL(to_plot):
-    return (round(((find_lowest(to_plot)) - (.1 * (find_highest(to_plot) - find_lowest(to_plot))))*100)/100)
+    return (round(((min(to_plot)) - (.1 * (max(to_plot) - min(to_plot))))*100)/100)
 def ploting_rangeH(to_plot):
-    return (round(((find_highest(to_plot)) + (.1 * (find_highest(to_plot) - find_lowest(to_plot))))*100)/100)
+    return (round(((max(to_plot)) + (.1 * (max(to_plot) - min(to_plot))))*100)/100)
 
 def Device_Efficiency_versus_Relative_Current_Density(list_name):
     #why are there three listSeebeck? not two get rid of u_vs...
     #Relative_Current_Density aka U
+    # not run
     U = []
     dev_eff = []
     x = 1
@@ -109,41 +109,42 @@ def Device_Efficiency_versus_Relative_Current_Density(list_name):
         u += .01
         dev_eff.append(x)
         U.append(u)
-    highest_dev_eff = find_highest(dev_eff)
-    for position in range(len(dev_eff)):
-        if dev_eff[position] == highest_dev_eff:
-            hDF_U = U[position]
-            break
+    highest_dev_eff = max(dev_eff)
+    hDF_U = find_place(highest_dev_eff,dev_eff)
     
-    optimized_U = optomiseUfordefeff(list_name)
+    optimized_U = optimizedU(list_name)
     x = functions_efficiency_as_a_function_of_u(list_name, optimized_U)
     
   
     plt.plot(U, dev_eff,color="#065f00", linewidth=1.5)
-    print ("optimised u", optimized_U)
+    print ("optmissed u", optimized_U)
     print ("Highest device efficiency", x)
     plt.plot(hDF_U, highest_dev_eff, 'o', color="#821cff", linewidth=10,)
-    plt.axis([((find_lowest(U)) - .01), ((find_highest(U)) + .01), 0, ploting_rangeH(dev_eff)])
+    plt.axis([((min(U)) - .01), ((max(U)) + .01), 0, ploting_rangeH(dev_eff)])
     plt.ylabel('Device Efficiency')
     plt.xlabel('Relative Current Density (u)')
     plt.show()
 
 def CalculateData(data):
-    for thing in data:
-        zT = functions_zT(thing[0], thing[1], thing[2], thing[3])
-        thing.append(zT)
+    for datum in data:
+        zT = functions_zT(datum[0], datum[1], datum[2], datum[3])
+        datum.append(zT)
     for datum in data:
         Red_eff = functions_max_Red_eff(datum[4])
         datum.append(Red_eff)
     for datum in data:
         S = (math.sqrt(1+datum[4])-1)/(datum[2]*datum[0]/1000000)
         datum.append(S)
-    for datum in (data):
+    for datum in data:
         if len(datum) < 8:
             datum.append(.123)
 
-    optimized_U = optomiseUfordefeff(data)
-    (functions_efficiency_as_a_function_of_u(data, optimized_U))
+    optimized_U = optimizedU(data)
+    #(functions_efficiency_as_a_function_of_u(data, optimized_U))
+
+    data[0][7] = optimized_U
+    for datum in range(len(data)-1):
+        data[datum + 1][7] = (1/(((1/data[datum][7])*math.sqrt(abs(1-2*data[datum][7]*data[datum][7]*(data[datum+1][1]*data[datum+1][3]+data[datum][1]*data[datum][3])*(10**-5)/2*(data[datum+1][0]-data[datum][0]))))-(data[datum+1][0]+data[datum][0])/2*(data[datum+1][2]-data[datum][2])*(10**-6)))
 
     for datum in data:
         redefficiency = ((datum[7]*(datum[2]-datum[7]*datum[1]*datum[3]*10)/10**6)/(datum[7]*datum[2]/10**6+1/datum[0]))
@@ -159,6 +160,7 @@ def CalculateData(data):
         data[datum+1].append(ZT)
         
 def PlotData(data):
+    # I wonder if this is faster then doing it the fancy way.
     All_tempatures = []
     All_R = []
     All_Seebeck = []
@@ -185,10 +187,10 @@ def PlotData(data):
     for datum in range(len(data)-1):
         All_efficiency.append(data[datum+1][10])
         All_ZT.append(data[datum+1][11])
-        
+
     temp_incroment = All_tempatures[2]-All_tempatures[1]
-    maxy = (find_highest(All_tempatures)) + temp_incroment
-    miny = (find_lowest(All_tempatures)) - temp_incroment
+    maxy = max(All_tempatures) + temp_incroment
+    miny = min(All_tempatures) - temp_incroment
         
     plt.plot(All_tempatures, All_R)
     plt.axis([miny, maxy, ploting_rangeL(All_R), ploting_rangeH(All_R)])
@@ -258,30 +260,24 @@ def PlotData(data):
     plt.show()
     #11
     
-    u_vs_dev_eff = []
     U = []
     dev_eff = []
     x = 1
     u = .01
     while x > 0:
-        x = (functions_efficiency_as_a_function_of_u(data, u))
-        u_vs_dev_eff.append([u, x])
+        x = functions_efficiency_as_a_function_of_u(data, u)
         u += .01
         dev_eff.append(x)
         U.append(u)
-    highest_dev_eff = find_highest(dev_eff)
-    for thing in u_vs_dev_eff:
-        if thing[1] == highest_dev_eff:
-            hDF_U = thing[0]
-            break
-    optimized_U = optomiseUfordefeff(data)
-    x = (functions_efficiency_as_a_function_of_u(data, optimized_U))
-    U_incroment = U[2]-U[1]
+
+    optimized_U = data[0][7]# look for optimized_U in CalculateData
+    x = functions_efficiency_as_a_function_of_u(data, optimized_U)
+    U_incroment = U[2]-U[1]# always .01?
     plt.plot(U, dev_eff,color="#065f00", linewidth=1.5)
-    print ("optimised u", optimized_U)
+    print ("optmissed u", optimized_U)
     print ("Highest device efficiency", x)
-    plt.plot(hDF_U, highest_dev_eff, 'o', color="#821cff", linewidth=10,)
-    plt.axis([((find_lowest(U)) - U_incroment), ((find_highest(U)) + U_incroment), 0, ploting_rangeH(dev_eff)])
+    plt.plot(optimized_U, x, 'o', color="#821cff", linewidth=10,)
+    plt.axis([((min(U)) - U_incroment), ((max(U)) + U_incroment), 0, ploting_rangeH(dev_eff)])
     plt.ylabel('Device Efficiency')
     plt.xlabel('Relative Current Density (u)')
     plt.show()
@@ -289,9 +285,7 @@ def PlotData(data):
 print ("Units:")
 print ("your units should be miliOhm centimeters, micro volts per kelvin and watts per meter kelvin")
 print ("the first column should have temperature the second Resistance the third Sebec and the last Conductivity")
-#CSV = Import("Examples\Example 1.csv")
-#CalculateData(CSV)
-#PlotData(CSV)
+
 while True:
     print("These are the instructions. Enter:")
     print("A) to create a new CSV file with calculated ZT and other values")
@@ -300,7 +294,7 @@ while True:
     print("Q) to end the program.")
     mode = input("What do you want to do?(A, B, C)\n")
     if mode.upper() == 'A':
-        fileName = input("What is the name of the file?\n")
+        fileName = input("What is the name and path of the file?\n")
         #Import
         CSV = Import(fileName)
         #Extend list
